9328bbac137343d36c974e67c769671902002e0f
Manish Patil <manish.patil@Eternus.local>
2017-07-06 11:12:00 +0530

    Added domain and mapping for ServiceMeterLogs

---
+++ Libraries/Nop.Core/Domain/SmartMeterLogs/SmartMeterLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Core.Domain.SmartMeterLogs
+{
+    public partial class SmartMeterLog : BaseEntity
+    {
+        public Guid DeviceID { get; set; }
+        public string Lattitude { get; set; }
+        public string Longitude { get; set; }
+        public double Consumption { get; set; }
+        public int TimeInterval { get; set; }
+        public DateTime TimeIntervalSetTime { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime LoggingTime { get; set; }
+    }
+}
---
+++ Libraries/Nop.Data/Mapping/SmartMeterLogs/SmartMeterLogMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Core.Domain.SmartMeterLogs;
+
+namespace Nop.Data.Mapping.SmartMeterLogs
+{
+    public class SmartMeterLogMap: NopEntityTypeConfiguration<SmartMeterLog>
+    {
+        public SmartMeterLogMap()
+        {
+            this.ToTable("SmartMeterLogs");
+            this.HasKey(c => c.Id);
+            this.Property(u => u.DeviceID).IsRequired();
+            this.Property(u => u.Lattitude).IsRequired();
+            this.Property(u => u.Longitude).IsRequired();
+            this.Property(u => u.Consumption).IsRequired();
+            this.Property(u => u.TimeInterval).IsRequired();
+            this.Property(u => u.TimeIntervalSetTime).IsRequired();
+            this.Property(u => u.IsActive).IsRequired();
+            this.Property(u => u.LoggingTime).IsRequired();
+        }
+}
