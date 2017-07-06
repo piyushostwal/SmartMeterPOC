b720c0c62a251d7bca1e325ee5e3208c0a3062e6
Piyush Ostwal <Piyush.Ostwal>
2017-07-06 15:35:03 +0530

    Added Domain and Mapping class for CustomerProductDetails and CustomerBilling

---
+++ Libraries/Nop.Core/Domain/Customers/CustomerProductDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Nop.Core.Domain.Common;
+using Nop.Core.Domain.Orders;
+
+namespace Nop.Core.Domain.Customers
+{
+    public class CustomerProductDetails : BaseEntity
+    {
+        #region
+        public CustomerProductDetails()
+        { }
+        #endregion
+
+        #region Proeprties
+        public int OrderId { get; set; }
+        public Guid DeviceId { get; set; }
+        public string BillingUnit { get; set; }
+        public bool Status { get; set; }
+        public virtual Order Order { get; set; }
+        #endregion
+
+    }
+}
