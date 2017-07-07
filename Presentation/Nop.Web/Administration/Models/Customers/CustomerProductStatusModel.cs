e0afdf1be182c9b4526c0b2be0f0a5643b331a2a
Manish Patil <manish.patil@Eternus.local>
2017-07-07 17:47:10 +0530

    Added method to update status in CustomerProductDetails Table

---
+++ Presentation/Nop.Web/Administration/Models/Customers/CustomerProductStatusModel.cs
@@ -0,0 +1,14 @@
+﻿using Nop.Web.Framework.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nop.Admin.Models.Customers
+{
+    public class CustomerProductStatusModel : BaseNopModel
+    {
+        public Guid DeviceId { get; set; }
+        public bool Status { get; set; }
+    }
+}
