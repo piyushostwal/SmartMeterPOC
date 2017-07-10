0b7a7f0659eb7cfc88209b3e80c4d402aaf38daf
Manish Patil <manish.patil@Eternus.local>
2017-07-10 20:42:30 +0530

    Added APIs for fetching data for multiple device IDs and searching deviceIds

---
+++ Presentation/Nop.Web/Models/SmartMeterLogs/SmartMeterSearchModel.cs
@@ -0,0 +1,15 @@
+﻿using Nop.Web.Framework.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nop.Web.Models.SmartMeterLogs
+{
+    public class SmartMeterSearchModel : BaseNopEntityModel
+    {
+        public Guid DeviceId { get; set; }
+        public string CustomerName { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
