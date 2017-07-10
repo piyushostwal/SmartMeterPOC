8ceaadef2ce1bd5db148cb3e28c7ea1cc9b2ae0f
aslam.shrimali <aslam.shrimali@eternussolutions.com>
2017-07-10 16:58:32 +0530

    Added Activate/Deactivate meters functionality from Admin

--- Presentation/Nop.Web/Administration/Models/Customers/CustomerMeterDetails.cs
+++ Presentation/Nop.Web/Administration/Models/Customers/CustomerMeterDetails.cs
@@ -7,6 +7,19 @@ namespace Nop.Admin.Models.Customers
 {
     public class CustomerMeterDetails
     {
-
+        public string meterId { get; set; }
+        public string customerName { get; set; }
+        public string location { get; set; }
+        public string BillingUnit { get; set; }
+        public int ConsumptionUnitReading { get; set; }
+        public string BillPeriodFrom { get; set; }
+        public string BillPeriodTo { get; set; }
+        public decimal BillAmount { get; set; }
+        public string BillPaymentDate { get; set; }
+        public string BillDueDate { get; set; }
+        public int PreviousConsumptionUnitReading { get; set; }
+        public decimal? LastBillAmount { get; set; }
+        public string LastBillPaymentDate { get; set; }
+        
     }
 }
